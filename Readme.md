43369d99024dc6c6fc2fa82e8d7d7290b764b691
luka5 <github@luka5.de>
2013-03-16 01:34:42 +0100

    initial commit

---
+++ Readme.md
@@ -0,0 +1,18 @@
+
+# window-size
+
+  Returns the height and width of an window
+
+  Based on http://www.howtocreate.co.uk/tutorials/javascript/browserwindow
+
+## Installation
+
+    $ component install luka5/window-size
+
+## API
+
+   
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,23 @@
+exports = module.exports = function () {
+  var size = {width: 0, height: 0}
+  if( typeof( window.innerWidth ) == 'number' ) {
+    //Non-IE
+    size.width = window.innerWidth;
+    size.height = window.innerHeight;
+  } else if( document.documentElement && ( document.documentElement.clientWidth || document.documentElement.clientHeight ) ) {
+    //IE 6+ in 'standards compliant mode'
+    size.width = document.documentElement.clientWidth;
+    size.height = document.documentElement.clientHeight;
+  } else if( document.body && ( document.body.clientWidth || document.body.clientHeight ) ) {
+    //IE 4 compatible
+    size.width = document.body.clientWidth;
+    size.height = document.body.clientHeight;
+  }
+  return size;
+};
+exports.height = function () {
+  return exports().height;
+};
+exports.width = function () {
+  return exports().width;
+};
